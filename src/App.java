c604443ff01f2f60a1aae4e554d670323a5df319
matlam27 <117271454+matlam27@users.noreply.github.com>
2023-01-09 12:16:51 +0100

    test

--- src/App.java
+++ src/App.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
     public static void afficher_menu(){
-        
+        String touche = scanner.nextline();
+        if touche == "q"{
+            System.out.println("Hello, World!"); 
+        }
     }
 }
