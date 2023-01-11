5d938a0e2f849ee3e6c199c365a04761748c3061
matlam27 <117271454+matlam27@users.noreply.github.com>
2023-01-11 16:41:50 +0100

    supprimer et recherche contact

--- src/App.java
+++ src/App.java
@@ -1,20 +1,35 @@
+import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.function.Predicate;
 
 import model.Contact;
 
 public class App {
+
     private static Scanner _scan = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
-        while(true){
+
+        while (true) {
             afficherMenu();
             String choix = _scan.nextLine();
             switch (choix) {
-                case "1": 
+                case "1":
                     ajouterContact();
                     break;
-                case "2": 
+                case "2":
+                    listerContacts();
+                case "3":
+                    break;
+                case "4":
+                    System.out.print("Email ? : ");
+                    contactsuppr(_scan.nextLine());
+                    break;
+                case "5": 
+                    System.out.print("Saisir le prénom: ");
+                    Contact.RechercherPrenom(_scan.nextLine());
                     break;
                 case "q":
                     return;
@@ -24,39 +39,80 @@ public class App {
             }
         }
     }
-    private static void ajouterContact(){
+
+    private static void listerContacts() {
+        try {
+            ArrayList<Contact> list = Contact.lister();
+
+            for (Contact contact : list) {
+                System.out.println(contact.getNom() + " " + contact.getPrenom());
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void ajouterContact() throws IOException {
         Contact c = new Contact();
         System.out.println("Saisir le nom");
+        c.setNom(_scan.nextLine());
+
+        System.out.println("Saisir le prénom");
         c.setPrenom(_scan.nextLine());
 
-        System.out.println("Saisir le prenom");
-        c.setPrenom(_scan.nextLine());
+        while (true) {
+            try {
+                System.out.println("Saisir le mail");
+                c.setMail(_scan.nextLine());
+                break;
+            } catch (ParseException e) {
+                System.out.println(e.getMessage());
+            }
+        }
 
-        System.out.println("Saisir l'email'");
-        c.setMail(_scan.nextLine());
+        while (true) {
+            try {
+                System.out.println("Saisir le téléphone");
+                c.setTelephone(_scan.nextLine());
+                break;
+            } catch (ParseException e) {
+                System.out.println("Mauvais téléphone!");
+            }
+        }
 
-        System.out.println("Saisir le numéro de téléphone");
-        c.setTelephone(_scan.nextLine());
-        
-        while(true){
-            try{
+        while (true) {
+            try {
                 System.out.println("Saisir la date de naissance");
                 c.setDateNaissance(_scan.nextLine());
                 break;
-            } catch (ParseException e){
-                System.out.println("Mauvaise date");
+            } catch (ParseException e) {
+                System.out.println("Mauvaise date de naissance!");
             }
         }
+        c.enregistrer();
         
+        System.out.println("Contact enregistré");
 
     }
-    public static void afficherMenu(){
-        ArrayList<String> menus =new ArrayList<>();
+    private static void contactsuppr(String contacttosupr) throws IOException, ParseException{
+        ArrayList<Contact> list = Contact.lister();
+        Predicate<Contact> condition = contact -> contact.getMail().startsWith(contacttosupr);
+
+        list.removeIf(condition);
+        Contact.refreshlist(list);
+        System.out.println(list);
+    }
+
+    private static void afficherMenu() {
+        ArrayList<String> menus = new ArrayList<>();
         menus.add("-- MENU --");
         menus.add("1- Ajouter un contact");
         menus.add("2- Lister les contacts");
+        menus.add("3- Modifier un contact");
+        menus.add("4- Supprimer un contact");
+        menus.add("5- Rechercher un contact par le prénom");
         menus.add("q- Quitter");
-        for (String menu :menus){
+        for (String menu : menus) {
             System.out.println(menu);
         }
     }
--- src/model/Contact.java
+++ src/model/Contact.java
@@ -1,47 +1,168 @@
 package model;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Contact {
+    private static final String SEPARATEUR = ";";
+
     private String nom;
-    private String  prenom;
+    private String prenom;
     private String mail;
     private String telephone;
     private Date dateNaissance;
+
     public String getNom() {
         return nom;
     }
+
     public void setNom(String nom) {
         this.nom = nom;
     }
+
     public String getPrenom() {
         return prenom;
     }
+
     public void setPrenom(String prenom) {
         this.prenom = prenom;
     }
+
     public String getMail() {
         return mail;
     }
-    public void setMail(String mail) {
-        this.mail = mail;
+
+    public void setMail(String mail) throws ParseException {
+        Pattern pat = Pattern.compile("^[a-zA-Z0-9_.-]+@{1}[a-zA-Z0-9_.-]{2,}\\.[a-zA-Z.]{2,10}$");
+        Matcher matcher = pat.matcher(mail);
+        if (matcher.matches()) {
+            this.mail = mail;
+        } else {
+            ParseException e = new ParseException("Le format du mail est incorrect.", 0);
+            throw e;
+        }
     }
+
     public String getTelephone() {
         return telephone;
     }
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
-    public Date getDateNaissance() {
-        return dateNaissance;
-    }
-    public void setDateNaissance(String dateNaissance) throws ParseException{  
-            SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
-            this.dateNaissance = format.parse(dateNaissance);
-        
+
+    public void setTelephone(String telephone) throws ParseException {
+        Pattern pat = Pattern.compile("^(?:(?:\\+|00)33|0)\\s*[1-9](?:[\\s.-]*\\d{2}){4}$");
+        Matcher matcher = pat.matcher(telephone);
+        if (matcher.matches()) {
+            this.telephone = telephone;
+        } else {
+            ParseException e = new ParseException("Le format du numéro est incorrect.", 0);
+            throw e;
+        }
     }
 
-    
+    public String getDateNaissance() {
+        SimpleDateFormat f = new SimpleDateFormat("dd/MM/yyyy");
+        return f.format(dateNaissance);
+    }
+
+    public void setDateNaissance(String dateNaissance) throws ParseException {
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        this.dateNaissance = format.parse(dateNaissance);
+    }
+
+    public void enregistrer() throws IOException {
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("contacts.csv", true)));
+        try {
+            pw.println(this.toString());
+        } finally {
+            pw.close();
+        }
+    }
+
+    public static ArrayList<Contact> lister() throws FileNotFoundException, IOException, ParseException{
+        ArrayList<Contact> list= new ArrayList<>();
+        try(BufferedReader buf = new BufferedReader(new FileReader("contacts.csv"))){
+            String ligne = buf.readLine();
+            while (ligne!= null){
+                String[] tab = ligne.split(SEPARATEUR);
+                Contact c =  new Contact();
+                c.setNom(tab[0]);
+                c.setPrenom(tab[1]);
+                c.setMail(tab[2]);
+                c.setTelephone(tab[3]);
+                c.setDateNaissance(tab[4]);
+                list.add(c);
+                ligne = buf.readLine();
+            }
+        }
+        return list;
+    }
+
+    public static ArrayList<Contact> RechercherPrenom(String prenom) throws IOException{
+        BufferedReader br = new BufferedReader(new FileReader("contacts.csv"));
+        ArrayList<Contact> list = new ArrayList<>();        
+        try{
+            String ligne = br.readLine();
+            while(ligne != null){
+                String[] table = ligne.split(";");
+                Contact contact = new Contact();
+
+                contact.setNom(table[0]);
+                contact.setPrenom(table[1]);
+                contact.setMail(table[2]);
+                contact.setTelephone(table[3]);
+                contact.setDateNaissance(table[4]);
+                if (table[1].equals(prenom)){
+                    System.out.println(contact.getNom() + " " + contact.getPrenom() + " " + contact.getMail() + " " + contact.getTelephone() + " " + contact.getDateNaissance());
+                    list.add(contact);
+                }
+
+                ligne = br.readLine();
+            }
+        }catch (ParseException | IOException e){
+            System.out.println("Error");
+
+        }finally{
+            br.close();
+        }
+
+        return list;
+    }
+
+    public static void refreshlist(ArrayList<Contact> list) throws IOException{
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("contacts.csv", false)));
+        try{
+            
+            for (Contact contact : list) {
+                pw.println(contact.toString());
+            }
+        }finally{
+            pw.close();
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder build = new StringBuilder();
+        build.append(this.getNom());
+        build.append(SEPARATEUR);
+        build.append(this.getPrenom());
+        build.append(SEPARATEUR);
+        build.append(this.getMail());
+        build.append(SEPARATEUR);
+        build.append(this.getTelephone());
+        build.append(SEPARATEUR);
+        build.append(this.getDateNaissance());
+        return build.toString();
+    }
+
 }
