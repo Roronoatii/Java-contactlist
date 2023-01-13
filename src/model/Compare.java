bfa1c4aa49a533610882c3b6e950646afb992e27
matlam27 <117271454+matlam27@users.noreply.github.com>
2023-01-13 16:34:21 +0100

    doc

--- src/model/Compare.java
+++ src/model/Compare.java
@@ -1,11 +1,25 @@
 package model;
 
 import java.util.Comparator;
-
+/** 
+ * classe Compare qui permet de comparer les données d'un contact 
+ */
 public class Compare implements Comparator<Contact> {
+    /**
+     * méthode qui  permet ici de comparer l'email d'un contact avec un deuxième contact
+     * @param c1
+     * @param c2
+     * @return
+     */
     public int compare(Contact c1, Contact c2){
         return c1.getBirthday().compareTo(c2.getBirthday());
     }
+    /**
+     * méthode qui  permet ici de comparer l'email d'un contact avec un deuxième contact
+     * @param c1
+     * @param c2
+     * @return
+     */
     public int compare2(Contact c1, Contact c2){
         return c1.getEmail().compareTo(c2.getEmail());
     }
